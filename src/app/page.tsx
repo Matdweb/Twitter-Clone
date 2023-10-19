ccb3c983970c88326d8bd6a954bd85144ca540c4
Matias Vargas <mat.dweb28[1>
2023-10-18 21:57:01 -0600

    feat: integrate buttons to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,11 @@ export default function Home() {
           4-kursni tugatgunimcha kamida bitta biznesim bo'lishini, uylanish uchun moddiy jihatdan to'la-to'kis tayyor bo'lishni, sog'lik va jismoniy holatni normallashtirishni reja qildim
         </p>
       </section>
-      
+      <section className='p-16 pt-4 bg-white'>
+        <button className='btn-primary w-40 m-4 ml-0 p-4'>Tweet</button>
+        <button className='btn-secondary w-40 m-4 ml-0 p-4'>Follow</button>
+        <button className='btn-edit w-40 m-4 ml-0 p-4'>Edit profile</button>
+      </section>
     </>
   )
 }
