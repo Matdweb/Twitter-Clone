8092ce79b21e7d6cb039f9ef80eedb079f6205b1
Matias Vargas <mat.dweb28[1>
2023-10-20 22:15:05 -0600

    style: add indentation to imports

--- src/redux/hook.ts
+++ src/redux/hook.ts
@@ -1,4 +1,4 @@
-import {useDispatch, useSelector, TypedUseSelectorHook} from 'react-redux'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 import { AppDispatch, RootState } from "./store"
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
