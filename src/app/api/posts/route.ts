f4054ea85342c8a3c10c53539d692b89c0ee1def
Matias Vargas <mat.dweb28[1>
2023-10-27 22:39:20 -0600

    feat(api): add '/api/posts' route for retrieving posts and images

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,38 @@
+import type { Post } from "@/types/posts/posts";
+import type { PiscumImage } from "@/types/posts/images/images";
+
+export async function POST(request: Request) {
+    const { lastPostId } = await request.json() || 0;
+    const batchSize = 12;
+    const postsLength = 100;
+    const endPost = Math.min(lastPostId + batchSize, postsLength);
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${lastPostId}&_end=${endPost}`);
+    const posts = await response.json();
+
+    const imageURLs = await requestImages(batchSize);
+
+    return Response.json({
+        posts: posts.map((post: Post, i: number) => {
+            return {
+                ...post,
+                imageURL: imageURLs[i]
+            }
+        })
+    });
+}
+
+const requestImages = async (batchSize: number) => {
+    const randomPage = Math.floor(Math.random() * 80) + 1;
+    try {
+        const response = await fetch(`https://picsum.photos/v2/list?page=${randomPage}&limit=${batchSize}`);
+        const imageURLs = await response.json();
+
+        return imageURLs.map((image: PiscumImage) => {
+            return image.download_url;
+        })
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+}
