ecd0f2908eb6953fb006dbb1dfe12ca15e8daea8
Matias Vargas <mat.dweb28[1>
2023-10-30 20:29:18 -0600

    feat(redux): Add postsReducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice";
+import postsReducer from './features/postsSlice'
 
 export const store = configureStore({ 
     reducer: {
-        counterReducer
+        counterReducer,
+        postsReducer
     }
 });
 
