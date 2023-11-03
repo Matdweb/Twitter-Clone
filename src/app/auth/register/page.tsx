a2e25d3794b9af094c5c79c69914b1b5913c8810
mat_dweb <mat.dweb28@gmail.com>
2023-11-03 17:43:43 -0600

    feat: create user registration page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,74 @@
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+function Page() {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(name, email, password);
+        try {
+            const response = await fetch('../api/auth/register',{
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name, email, password })
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (e){
+            console.log(e);
+        }
+    }
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
+    return (
+        <form onSubmit={onSubmit} className='p-16'>
+            <h2 className='font-primary-title-roboto mb-8'>Register User</h2>
+
+            <div className='flex justify-center items-start flex-col flex-nowrap gap-3'>
+                <input
+                    type="text"
+                    value={name}
+                    placeholder='name'
+                    onChange={handleNameChange}
+                    required
+                    className='bg-transparent text-primary-gray border border-primary-gray rounded-lg p-2'
+                />
+                <input
+                    type="email"
+                    value={email}
+                    placeholder='email@example.com'
+                    onChange={handleEmailChange}
+                    required
+                    className='bg-transparent text-primary-gray border border-primary-gray rounded-lg p-2'
+                />
+                <input
+                    type="password"
+                    value={password}
+                    placeholder='password'
+                    onChange={handlePasswordChange}
+                    required
+                    className='bg-transparent text-primary-gray border border-primary-gray rounded-lg p-2'
+                />
+                <button type="submit" className='bg-white text-black p-2 rounded-lg w-20'>Register</button>
+                <Link href='../' className='bg-white text-black p-2 rounded-lg w-20'>Back</Link>
+            </div>
+        </form>
+    )
+}
+
+export default Page
