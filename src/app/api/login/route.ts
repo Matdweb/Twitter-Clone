88e9e33ea6bbf8db0b529133433c6aca1ebf139b
mat_dweb <mat.dweb28@gmail.com>
2023-11-03 22:40:42 -0600

    feat: create 'api/login' endpoint for credential validation

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,22 @@
+import findUserByEmail from "@/lib/findUserByEmail";
+import { signIn } from "next-auth/react";
+
+export async function POST(request: Request) {
+    const {
+        email,
+        password
+    } = await request.json();
+
+    const user = await findUserByEmail(email);
+    
+    if (user) {
+        if (user.password === password) {
+            return Response.json({status: 201, statusText: 'Authenticated User'});
+        } else {
+            return Response.json({status: 400, statusText: 'Wrong password'})
+        }
+    } else {
+        return Response.json({ status: 400, statusText: 'User doesnt exists' });
+    }
+
+}
