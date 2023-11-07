1f318d2d87e564e36556c31af0ff59120a183407
Matias Vargas <mat.dweb28[1>
2023-11-06 23:17:45 -0600

    feat: hash passwords before storing in the database

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,6 +1,7 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 import findUserByEmail from "@/lib/findUserByEmail";
+import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
     const {
@@ -9,14 +10,16 @@ export async function POST(request: Request) {
         password
     } = await request.json();
 
-    console.log(name, email, password);
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    console.log(name, email, hashedPassword);
 
     if (await findUserByEmail(email)) {
         return Response.json({ status: 400, statusText: 'There is already a User' });
     }
 
     await connectMongoDB();
-    await User.create({ name, email, password, posts: [] });
+    await User.create({ name, email, password: hashedPassword, posts: [] });
 
     return Response.json({ status: 201, statusText: 'User creation successfull' });
 }
