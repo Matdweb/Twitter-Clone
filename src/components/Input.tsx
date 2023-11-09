7822a37f21c5ce2c42a266ebffbb8b0001e1123d
Matias Vargas <mat.dweb28[1>
2023-11-09 14:05:31 -0600

    feat: develop captivating and functional login page

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type InputProps = React.ComponentProps<"input"> & {
+    error: string | boolean
+};
+
+export default function Input({ error, ...rest }: InputProps) {
+    return (
+        <input
+            {...rest}
+            className={`w-full bg-transparent p-5 mb-5 outline outline-1  ${error ? `outline-primary-red` : `outline-primary-gray`} rounded-md p-2 focus-visible:outline-primary-blue dark:focus-visible:outline-white dark:focus-visible:outline-2`}
+        />
+    )
+}
