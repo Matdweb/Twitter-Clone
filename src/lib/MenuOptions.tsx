18c5f7fb6c041adbbbafcd013bdaf44fe2f4471e
mat_dweb <mat.dweb28@gmail.com>
2023-11-13 23:40:57 -0600

    feat: create exportable object for sideMenuOptions

---
+++ src/lib/MenuOptions.tsx
@@ -0,0 +1,71 @@
+import { BiHomeCircle } from 'react-icons/bi';
+import { BiSolidHomeAlt2 } from 'react-icons/bi';
+import { HiHashtag } from 'react-icons/hi2';
+import { HiMiniHashtag } from 'react-icons/hi2';
+import { BsBell } from 'react-icons/bs';
+import { BsBellFill } from 'react-icons/bs';
+import {BiMessageAltDetail} from 'react-icons/bi';
+import {BiSolidMessageAltDetail} from 'react-icons/bi';
+import {BsPerson} from 'react-icons/bs';
+import {BsPersonFill} from 'react-icons/bs';
+import {CiCircleMore} from 'react-icons/ci';
+import {CgMoreO} from 'react-icons/cg';
+
+export const menuOptions = [
+    {
+        id: 0,
+        name: "Home",
+        href: '../home',
+        icon: {
+            ligth: <BiHomeCircle style={{ fontSize: "1.75rem" }} />,
+            bold: <BiSolidHomeAlt2 style={{ fontSize: "1.75rem" }} />
+        },
+        active: true
+    },
+    {
+        id: 1,
+        name: "Explore",
+        icon: {
+            ligth: <HiHashtag style={{ fontSize: "1.75rem" }} />,
+            bold: <HiMiniHashtag style={{ fontSize: "1.75rem" }} />
+        },
+        active: false
+    },
+    {
+        id: 2,
+        name: "Notifications",
+        icon: {
+            ligth: <BsBell style={{ fontSize: "1.5rem" }} />,
+            bold: <BsBellFill style={{ fontSize: "1.5rem" }} />
+        },
+        active: false
+    },
+    {
+        id: 3,
+        name: "Messages",
+        icon: {
+            ligth: <BiMessageAltDetail style={{ fontSize: "1.75rem" }} />,
+            bold: <BiSolidMessageAltDetail style={{ fontSize: "1.75rem" }} />
+        },
+        active: false
+    },
+    {
+        id: 4,
+        name: "Profile",
+        href: "home/MatDweb",
+        icon: {
+            ligth: <BsPerson style={{ fontSize: "1.75rem" }} />,
+            bold: <BsPersonFill style={{ fontSize: "1.75rem" }} />
+        },
+        active: false
+    },
+    {
+        id: 5,
+        name: "More",
+        icon: {
+            ligth: <CiCircleMore style={{ fontSize: "1.75rem" }} />,
+            bold: <CgMoreO style={{ fontSize: "1.75rem" }} />
+        },
+        active: false
+    }
+]
