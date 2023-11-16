8ca4a8942afd59e9a0afc954cf2c05e960d35f3b
mat_dweb <mat.dweb28@gmail.com>
2023-11-15 23:01:23 -0600

    feat: seamlessly integrate new components into PopularTrendsSection

--- src/components/TrendsSection/PopularTrendsSection.tsx
+++ src/components/TrendsSection/PopularTrendsSection.tsx
@@ -1,8 +1,18 @@
+import TwitterSearcher from "./TwitterSearcher";
+import PopularTrends from "./PopularTrends";
+import WhoToFollow from "./WhoToFollow";
+import Footer from "./Footer";
 
 function PopularTrendsSection() {
+
     return (
-        <section className='w-1/3 min-h-screen outline outline-2 outline-black dark:outline-white rounded-md p-1 m-1 shrink'>
-            <h2 className='font-primary-title-roboto'>Trends</h2>
+        <section className='w-1/3 min-h-screen outline outline-2 rounded-md p-1 m-1 shrink flex justify-start items-start flex-col flex-nowrap'>
+            <TwitterSearcher />
+            <div className='w-full px-7 flex justify-center items-stretch flex-col'>
+                <PopularTrends />
+                <WhoToFollow />
+                <Footer />
+            </div>
         </section>
     )
 }
