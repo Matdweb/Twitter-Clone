13e8c43129b8a7227fb3b6f58e11c28adb270df0
mat_dweb <mat.dweb28@gmail.com>
2023-11-15 22:48:05 -0600

    feat(component): craft an inspiring responsive transformation for the SideMenu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { useState } from "react"
-import { useAppSelector, useAppDispatch } from '@/redux/hook';
-import { toggleResponsiveMenu } from '@/redux/features/responsiveMenuSlice';
+import { useAppSelector } from '@/redux/hook';
 import { menuOptions as Options } from "@/lib/MenuOptions";
 import type { MenuOptionType } from '@/types/MenuOptions/menuOptionType';
 import { TbPencilPlus } from 'react-icons/tb';
 import TwitterIcon from '../TwitterIcon';
 import MenuOption from "./MenuOption";
 import UserAccout from "./UserAccout";
+import ModeToggleButton from "../ModeToggleButton";
 
 function SideMenu() {
     const [menuOptions, setMenuOptions] = useState<MenuOptionType[]>(Options);
@@ -36,15 +36,23 @@ function SideMenu() {
 
     return (
         <section className={`${windowWidth < 640 && !responsiveMenu ? `hidden` : windowWidth < 1024 ? `w-16 p-2` : `w-1/4`} ${responsiveMenu && `min-w-[16rem]`} min-h-screen outline outline-2 outline-black dark:outline-white rounded-md p-1 m-1 shrink flex justify-between items-start flex-col flex-nowrap`}>
+            {
+                responsiveMenu &&
+                <UserAccout />
+            }
+            <nav className={`${windowWidth > 1024 && !responsiveMenu ? `w-60` : `w-full`} max-h-[37rem] flex items-start flex-nowrap flex-col`}>
 
-            <nav className={`${windowWidth > 1024 || responsiveMenu ? `w-60` : `w-full`} max-h-[37rem] mt-8 flex items-start flex-nowrap flex-col`}>
-                <TwitterIcon fontSize="2.5rem" marginLeft={windowWidth > 1024 ? "1rem" : "0.3rem"} marginBottom="2rem" />
+                {
+                    !responsiveMenu &&
+                    <TwitterIcon fontSize="2.5rem" marginLeft={windowWidth > 1024 ? "1rem" : "0.3rem"} marginBottom="2rem" marginTop="2rem" />
+                }
 
                 {
                     menuOptions.map(({ id, icon, href, active, name }) => {
                         return (
                             <MenuOption
-                                key={id} id={id}
+                                key={id} 
+                                id={id}
                                 href={href}
                                 name={name}
                                 icon={icon}
@@ -55,14 +63,15 @@ function SideMenu() {
                     })
                 }
 
-                <button className='btn-primary mt-4 w-full p-4'>
+                <button className='btn-primary mt-4 w-full p-4 text-white dark:text-white'>
                     {windowWidth > 1024 || responsiveMenu ?
                         'Tweet' :
                         <TbPencilPlus style={{ fontSize: "1rem" }} />}
                 </button>
             </nav>
 
-            <UserAccout />
+            {!responsiveMenu && <UserAccout />}
+            {responsiveMenu && <ModeToggleButton className='pt-3' />}
         </section>
     )
 }
--- src/components/SideMenu/UserAccout.tsx
+++ src/components/SideMenu/UserAccout.tsx
@@ -8,34 +8,43 @@ function UserAccout() {
     const dispatch = useAppDispatch();
 
     return (
-        <div 
-        className={`rounded-full cursor-pointer ${windowWidth > 1024 ? `w-60 p-3 mt-3 hover:bg-black/20 dark:hover:bg-white/20` : `w-full`} flex justify-between items-center flex-row`} 
-        onClick={responsiveMenu ? ()=> dispatch(toggleResponsiveMenu()) : ()=> {}}
-        >
-            {
-                windowWidth > 1024 || responsiveMenu ?
-                    <>
-                        <div className='flex justify-start items-center flex-row flex-nowrap'>
-                            <img
-                                src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
-                                alt="profile-image"
-                                className='w-12 h-12 rounded-full mr-2'
-                            />
-                            <div className='flex justify-center items-start flex-col flex-nowrap'>
-                                <p className='font-bold'>Mat.dweb</p>
-                                <p className='font-gray-text'>@MatDweb28</p>
+        <>
+            <div
+                className={`rounded-full mt-3 cursor-pointer ${windowWidth > 1024 ? `w-60 p-3 hover:bg-primary-gray dark:hover:bg-primary-dark-gray` : `w-full`} flex ${responsiveMenu ? `justify-start items-start flex-col pl-3` : `justify-between items-center flex-row`}`}
+                onClick={responsiveMenu ? () => dispatch(toggleResponsiveMenu()) : () => { }}
+            >
+                {
+                    windowWidth > 1024 || responsiveMenu ?
+                        <>
+                            <div className={`flex justify-start flex-nowrap ${responsiveMenu ? `items-start flex-col` : `items-center flex-row`}`}>
+                                <img
+                                    src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
+                                    alt="profile-image"
+                                    className='w-12 h-12 rounded-full mr-2 mb-1'
+                                />
+                                <div className='mt-1 flex justify-center items-start flex-col flex-nowrap'>
+                                    <p className='font-bold'>Mat.dweb</p>
+                                    <p className='font-gray-text'>@MatDweb28</p>
+                                </div>
                             </div>
-                        </div>
-                        <MdOutlineMoreHoriz style={{ marginRight: ".5rem", marginLeft: ".5rem" }} />
-                    </>
-                    :
-                    <img
-                        src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
-                        alt="profile-image"
-                        className='w-full rounded-full'
-                    />
-            }
-        </div>
+                            {!responsiveMenu && <MdOutlineMoreHoriz style={{ marginRight: ".5rem", marginLeft: ".5rem" }} />}
+                        </>
+                        :
+                        <img
+                            src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
+                            alt="profile-image"
+                            className='w-full rounded-full'
+                        />
+                }
+                {responsiveMenu &&
+                    <div className='mt-4 w-52 flex'>
+                        <p className='mr-1'>212</p>
+                        <p className='font-gray-text mr-2'>Followers</p>
+                        <p className='mr-1'>189</p>
+                        <p className='font-gray-text'>Following</p>
+                    </div>}
+            </div>
+        </>
     )
 }
 
