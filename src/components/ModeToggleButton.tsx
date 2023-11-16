1a367030067d39d3498efe96156439bb30bf0e72
mat_dweb <mat.dweb28@gmail.com>
2023-11-15 22:49:56 -0600

    feat(component): adapt ModeToggleButton for the responsive menu

--- src/components/ModeToggleButton.tsx
+++ src/components/ModeToggleButton.tsx
@@ -17,18 +17,18 @@ function ModeToggleButton({ className }: ButtonProps) {
 
   return (
     <div className={className}>
-      <h3 className='font-primary-title-bold text-black dark:text-white mb-10'>
+      {/* <h3 className='font-primary-title-bold text-black dark:text-white mb-10'>
         {theme === 'ligth' ? 'Dark' : 'Ligth'} Mode
-      </h3>
+      </h3> */}
 
       <section className='w-full flex justify-center items-center'>
         <div
-          className='w-20 h-20 rounded-md shadow-md shadow-black bg-black dark:bg-white dark:shadow-white flex justify-center items-center cursor-pointer'
+          className='w-10 h-10 rounded-md bg-black dark:bg-white dark:shadow-white flex justify-center items-center cursor-pointer'
           onClick={() => toggleMode()}
         >
           {theme === 'ligth' ?
-            <BsFillMoonFill style={{ color: "white", fontSize: "2.5em" }} /> :
-            <BsFillSunFill style={{ color: "black", fontSize: "2.5em" }} />}
+            <BsFillMoonFill style={{ color: "white", fontSize: "1.5em" }} /> :
+            <BsFillSunFill style={{ color: "black", fontSize: "1.5em" }} />}
         </div>
       </section>
     </div>
