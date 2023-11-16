63c3ed2e8710605e5b19cdd20132278cd62fa611
mat_dweb <mat.dweb28@gmail.com>
2023-11-15 22:50:26 -0600

    chore(component): gracefully remove ModeToggleButton from  page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useEffect } from 'react'
-import ModeToggleButton from '@/components/ModeToggleButton';
 import { useAppSelector, useAppDispatch } from '@/redux/hook';
 import { setWindowWidth } from '@/redux/features/windowWidthSlice';
 import SideMenu from '@/components/SideMenu/SideMenu';
@@ -28,19 +27,16 @@ function HomeLayout({
     }, [])
 
     return (
-        <>
-            <section className='w-full min-h-screen bg-white dark:bg-black text-black dark:text-white flex justify-start items-start overflow-x-hidden'>
-                <SideMenu />
+        <section className='w-full min-h-screen bg-white dark:bg-black text-black dark:text-white flex justify-start items-start overflow-x-hidden'>
+            <SideMenu />
 
-                {children}
+            {children}
 
-                {windowWidth > 768 &&
-                    <PopularTrendsSection />
-                }
+            {windowWidth > 768 &&
+                <PopularTrendsSection />
+            }
 
-            </section>
-            <ModeToggleButton className='absolute scale-75 bottom-0 right-0 m-8' />
-        </>
+        </section>
     )
 }
 
