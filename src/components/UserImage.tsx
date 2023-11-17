d21139166bcc792d26d4ef78ebd50c0318dfebfd
Matias Vargas <mat.dweb28[1>
2023-11-16 19:37:10 -0600

    fix(style): resolve width issue in UserImage component

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -6,7 +6,7 @@ function UserImage({ className, onClick }: Props) {
         <img
             src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
             alt="profile-image"
-            className={`w-full rounded-full ${className}`}
+            className={`${className} rounded-full`}
             onClick={onClick}
         />
     )
