8d32daa3ca94ef01488c300838f23351b104cdfa
Matias Vargas <mat.dweb28[1>
2023-11-24 21:29:44 -0600

    chore: move BottomCreatePostOptions to a new folder

---
+++ src/components/PostsSection/CreatePost/BottomCreatePostOptions.tsx
@@ -0,0 +1,49 @@
+import { LuImage } from "react-icons/lu";
+import { MdOutlineGifBox } from "react-icons/md";
+import { IoStatsChartSharp } from "react-icons/io5";
+import { GrEmoji } from "react-icons/gr";
+import { FaRegCalendarMinus } from "react-icons/fa";
+
+export const bottomCreatePostOptions = [{
+    id: 0,
+    Icon: LuImage,
+    style: {
+        fontSize: "1.5rem",
+        cursor: " pointer",
+    },
+    onClick: () => { }
+},
+{
+    id: 1,
+    Icon: MdOutlineGifBox,
+    style: {
+        fontSize: "1.5rem",
+        cursor: " pointer",
+    },
+    onClick: () => { }
+},
+{
+    id: 2,
+    Icon: IoStatsChartSharp,
+    style: {
+        fontSize: "1.5rem",
+        cursor: " pointer",
+    },
+    onClick: () => { }
+}, {
+    id: 3,
+    Icon: GrEmoji,
+    style: {
+        fontSize: "1.5rem",
+        cursor: " pointer",
+    },
+    onClick: () => { }
+}, {
+    id: 4,
+    Icon: FaRegCalendarMinus,
+    style: {
+        fontSize: "1.5rem",
+        cursor: " pointer",
+    },
+    onClick: () => { }
+}];
