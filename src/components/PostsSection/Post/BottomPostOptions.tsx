64f5dc775f85a50c982204331a4f6e936adcabd6
Matias Vargas <mat.dweb28[1>
2023-11-24 21:24:14 -0600

    chore: rename BottomMenuOptions to BottomPostOptions for clarity

---
+++ src/components/PostsSection/Post/BottomPostOptions.tsx
@@ -0,0 +1,77 @@
+import { FaRegComment } from "react-icons/fa6";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { GoHeart } from "react-icons/go";
+import { GoHeartFill } from "react-icons/go";
+import { RxShare2 } from "react-icons/rx";
+import { IoIosStats } from "react-icons/io";
+
+export const bottomPostOptions = [{
+    id: 0,
+    name: "comments",
+    icons: [{
+        Icon: FaRegComment,
+        style: {
+            fontSize: "1rem",
+        }
+    }],
+    clickable: false,
+    active: false
+},
+{
+    id: 1,
+    name: "retweets",
+    icons: [{
+        Icon: AiOutlineRetweet,
+        style: {
+            fontSize: "1rem",
+        }
+    },
+    {
+        Icon: AiOutlineRetweet,
+        style: {
+            fontSize: "1rem",
+            color: "lightgreen"
+        }
+    }],
+    clickable: true,
+    active: false
+},
+{
+    id: 2,
+    name: "likes",
+    icons: [{
+        Icon: GoHeart,
+        style: {
+            fontSize: "1rem",
+        }
+    },
+    {
+        Icon: GoHeartFill,
+        style: {
+            fontSize: "1rem",
+            color: "#EF1C5C"
+        }
+    }],
+    clickable: true,
+    active: false
+}, {
+    id: 3,
+    name: "share",
+    icons: [{
+        Icon: RxShare2,
+        style: {
+            fontSize: "1rem",
+        }
+    }],
+    clickable: false,
+}, {
+    id: 4,
+    name: "stats",
+    icons: [{
+        Icon: IoIosStats,
+        style: {
+            fontSize: "1rem",
+        }
+    }],
+    clickable: false,
+}];
