d411096f33886f909b4eb669e08c1c57b8ad4dd4
Matias Vargas <mat.dweb28[1>
2023-11-30 22:15:08 -0600

    feat(UserImage): add default image when src is empty

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -1,15 +1,29 @@
 
-type Props = React.ComponentProps<"img">;
+type Props = React.ComponentProps<"img"> & {
+    username?: string
+};
 
-function UserImage({ className, onClick }: Props) {
-    return (
-        <img
-            src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
-            alt="profile-image"
-            className={`${className} rounded-full`}
-            onClick={onClick}
-        />
-    )
+function UserImage({ className, username, src = "", onClick }: Props) {
+    if (src) {
+        return (
+            <img
+                src="https://pbs.twimg.com/profile_images/1546362031854559232/_vKO9a8v_400x400.jpg"
+                alt="profile-image"
+                className={`${className} rounded-full`}
+                onClick={onClick}
+            />
+        )
+    } else {
+        const firstLetter = username?.charAt(0).toUpperCase();
+        return (
+            <div
+                className={`${className} rounded-full flex justify-center items-center border border-white bg-primary-gray`}
+                onClick={onClick}
+            >
+                <h3 className='font-primary-title-bold text-lg text-black dark:text-black'>{firstLetter || "U"}</h3>
+            </div>
+        )
+    }
 }
 
 export default UserImage
