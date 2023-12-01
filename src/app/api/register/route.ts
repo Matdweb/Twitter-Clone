278cb7ef27babff6cb4526fcef5ea04a35e276cb
Matias Vargas <mat.dweb28[1>
2023-11-30 22:00:35 -0600

    feat(api/register): update User data with profileImage, bio, country, followers, and following fields

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -8,17 +8,31 @@ export async function POST(request: Request) {
         username,
         name,
         email,
-        password
+        password,
+        country
     } = await request.json();
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     if (await findUserByEmail(email)) {
         return Response.json({ status: 400, statusText: 'The user already exists' });
     }
 
     await connectMongoDB();
-    await User.create({ username, name, email, password: hashedPassword, posts: [] });
+    await User.create({
+        username,
+        name,
+        email,
+        password: hashedPassword,
+        country,
+        profileImage: {
+            url: ""
+        },
+        bio: "",
+        posts: [],
+        followers: 0,
+        following: 0,
+    });
 
     return Response.json({ status: 201, statusText: 'User creation successfull' });
 }
