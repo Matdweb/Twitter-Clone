6659fb909eaaf12544bc31cc36bd916084e063cf
Matias Vargas <mat.dweb28[1>
2023-12-05 00:24:21 -0600

    style(FormInput): add styles for disabled Input component

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -8,7 +8,7 @@ export default function FormInput({ error=false, className="", ...rest }: InputP
     return (
         <input
             {...rest}
-            className={`w-full bg-transparent p-5 mb-5 outline outline-1  ${error ? `outline-primary-red` : `outline-primary-gray`} rounded-md p-2 focus-visible:outline-primary-blue dark:focus-visible:outline-white dark:focus-visible:outline-2 ${className}`}
+            className={`w-full bg-transparent p-5 mb-5 outline outline-1  ${error ? `outline-primary-red` : `outline-primary-gray`} rounded-md p-2 focus-visible:outline-primary-blue dark:focus-visible:outline-white dark:focus-visible:outline-2 disabled:opacity-30 ${className}`}
         />
     )
 }
