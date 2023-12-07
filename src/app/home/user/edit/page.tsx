074659b88a5b71ac9ddfdce9457e54316e11703a
Matias Vargas <mat.dweb28[1>
2023-12-06 22:50:26 -0600

    feat(/edit): integrate updateUser functionality for updating profile

--- src/app/home/user/edit/page.tsx
+++ src/app/home/user/edit/page.tsx
@@ -1,140 +1,171 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TwitterHeader from '@/components/Header/TwitterHeader';
 import FormInput from '@/components/Form/FormInput';
-import { useAppSelector } from '@/redux/hook';
+import { useAppSelector, useAppDispatch } from '@/redux/hook';
 import Link from 'next/dist/client/link';
-import { BiObjectsHorizontalCenter } from 'react-icons/bi';
+import { useRouter } from 'next/navigation';
+import { fetchUser } from '@/redux/features/userSlice';
+import { toggleResponsiveMenu } from '@/redux/features/responsiveMenuSlice';
+import updateUser from '@/lib/updateUser';
 
 function Page() {
 
     const responsiveMenu = useAppSelector(state => state.responsiveMenu);
     const user = useAppSelector(state => state.userReducer.user);
+    const dispatch = useAppDispatch();
+
+    const rounter = useRouter();
 
     const [name, setName] = useState<string>(user?.name || '');
     const [username, setUsername] = useState<string>(user?.username || '');
     const [country, setCountry] = useState<string>(user?.country || '');
     const [bio, setBio] = useState<string>(user?.bio || '');
+    const [error, setError] = useState<string>('');
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        await handleUpdateUser();
     }
+
+    const handleUpdateUser = async () => {
+        const email = user?.email || "";
+
+        const { status, statusText } = await updateUser({
+            name,
+            username,
+            email,
+            country,
+            bio
+        });
+
+        if (status !== 201) {
+            setError(statusText);
+        } else {
+            dispatch(fetchUser(email));
+            rounter.push('/home/user/profile')
+        }
+    }
+
+    const InputStyles = {
+        padding: '0.7rem'
+    };
 
     return (
         <>
             <TwitterHeader section='Edit Profile' />
-            <form onSubmit={handleSubmit} className='p-10 sm:p-0 w-full mt-14 sm:mt-5  h-full flex justify-center items-center flex-col'>
-                <Link href="/home/user/profile" className=' place-self-start sm:ml-5 mb-4 underline'>
-                    <p>Back</p>
-                </Link>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>User Image</p>
-                    </label>
-                    <FormInput
-                        type="file"
-                        id='username'
-                        autoComplete="username"
-                        className=''
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Email:</p>
-                    </label>
-                    <FormInput
-                        type="email"
-                        value={user?.email || ""}
-                        placeholder='Email address'
-                        readOnly
-                        autoComplete="email"
-                        disabled
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Password:</p>
-                    </label>
-                    <FormInput
-                        type="password"
-                        value={'12345'}
-                        placeholder='Password'
-                        readOnly
-                        minLength={5}
-                        autoComplete="current-password"
-                        disabled
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Name:</p>
-                    </label>
-                    <FormInput
-                        type="text"
-                        value={name}
-                        placeholder='Full Name'
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                        autoComplete="username"
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Username:</p>
-                    </label>
-                    <FormInput
-                        type="text"
-                        value={username}
-                        placeholder='Username'
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                        autoComplete="username"
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Country:</p>
-                    </label>
-                    <FormInput
-                        type="text"
-                        value={country}
-                        placeholder='Country'
-                        onChange={(e) => setCountry(e.target.value)}
-                        required
-                        autoComplete="country"
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col pb-2'>
-                    <label htmlFor="username" className='pb-2'>
-                        <p>Bio:</p>
-                    </label>
-                    <FormInput
-                        type="text"
-                        value={bio}
-                        placeholder='Bio Description'
-                        onChange={(e) => setBio(e.target.value)}
-                        required
-                        autoComplete="Bio Description"
-                        className=''
-                        style={{ padding: '0.7rem' }}
-                    />
-                </div>
-                <button type="submit" className='btn-edit w-full sm:w-[19rem] py-3 mt-6 mb-1'>
-                    <h3 className='font-primary-title-bold'>Save Changes</h3>
-                </button>
-            </form>
+            <section className={`w-full mt-14 sm:mt-5 ${responsiveMenu && `cursor-pointer opacity-50`}`} onClick={() => responsiveMenu && dispatch(toggleResponsiveMenu())}>
+                <form onSubmit={handleSubmit} className='p-10 sm:p-0 w-full h-full flex justify-center items-center flex-col'>
+                    <Link href="/home/user/profile" className=' place-self-start sm:ml-5 mb-4 underline'>
+                        <p>Back</p>
+                    </Link>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="userImage" className='pb-2'>
+                            <p>User Image</p>
+                        </label>
+                        <FormInput
+                            id='userImage'
+                            type="file"
+                            autoComplete="username"
+                        />
+                    </div>
 
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="email" className='pb-2'>
+                            <p>Email:</p>
+                        </label>
+                        <FormInput
+                            id="email"
+                            type="email"
+                            value={user?.email || ""}
+                            placeholder='Email address'
+                            readOnly
+                            autoComplete="email"
+                            disabled
+                            style={InputStyles}
+                        />
+                    </div>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="password" className='pb-2'>
+                            <p>Password:</p>
+                        </label>
+                        <FormInput
+                            id='password'
+                            type="password"
+                            value={'12345'}
+                            placeholder='Password'
+                            readOnly
+                            minLength={5}
+                            autoComplete="current-password"
+                            disabled
+                            style={InputStyles}
+                        />
+                    </div>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="name" className='pb-2'>
+                            <p>Name:</p>
+                        </label>
+                        <FormInput
+                            id='name'
+                            type="text"
+                            value={name}
+                            placeholder='Full Name'
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                            autoComplete="username"
+                            style={InputStyles}
+                        />
+                    </div>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="username" className='pb-2'>
+                            <p>Username:</p>
+                        </label>
+                        <FormInput
+                            id='username'
+                            type="text"
+                            value={username}
+                            placeholder='Username'
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                            autoComplete="username"
+                            style={InputStyles}
+                        />
+                    </div>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col'>
+                        <label htmlFor="country" className='pb-2'>
+                            <p>Country:</p>
+                        </label>
+                        <FormInput
+                            id='country'
+                            type="text"
+                            value={country}
+                            placeholder='Country'
+                            onChange={(e) => setCountry(e.target.value)}
+                            required
+                            autoComplete="country"
+                            style={InputStyles}
+                        />
+                    </div>
+                    <div className='w-full sm:w-[19rem] flex justify-center items-start flex-col pb-2'>
+                        <label htmlFor="bio" className='pb-2'>
+                            <p>Bio:</p>
+                        </label>
+                        <FormInput
+                            id='bio'
+                            type="text"
+                            value={bio}
+                            placeholder='Bio Description'
+                            onChange={(e) => setBio(e.target.value)}
+                            autoComplete="Bio Description"
+                            style={InputStyles}
+                        />
+                    </div>
+                    <button type="submit" className='btn-edit w-full sm:w-[19rem] py-3 mt-6 mb-1'>
+                        <h3 className='font-primary-title-bold'>Save Changes</h3>
+                    </button>
+                    <p className='py-5 font-gray-text'>{error}</p>
+                </form>
+            </section>
         </>
     )
 }
