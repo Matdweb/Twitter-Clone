2f82020fb011e03fedf486fe410a87a0934e1ea9
mat_dweb <mat.dweb28@gmail.com>
2023-12-08 17:30:30 -0600

    feat(UserImage): add src prop if it is a valid one

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -3,17 +3,18 @@ import { useAppSelector } from "@/redux/hook";
 import Image from "next/image";
 
 type Props = React.ComponentProps<"img"> & {
-    username?: string
+    username?: string,
+    src?: string
 };
 
-function UserImage({ className, username, onClick }: Props) {
+function UserImage({ className, src, username, onClick }: Props) {
     const user = useAppSelector(state => state.userReducer.user);
-    const src = user?.profileImage.thumbnailUrl || user?.profileImage.url;
+    const imgSrc = src || user?.profileImage.thumbnailUrl || user?.profileImage.url;
 
-    if (src) {
+    if (imgSrc) {
         return (
             <Image
-                src={src}
+                src={imgSrc}
                 width={100}
                 height={100}
                 alt="profile-image"
