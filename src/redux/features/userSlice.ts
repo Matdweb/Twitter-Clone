2d5ffb08ad3d9b1c6bdcb11e4c222273b8a57304
Matias Vargas <mat.dweb28[1>
2023-12-15 00:40:58 -0600

    feat(redux/user): create addUserPost action reducer

--- src/redux/features/userSlice.ts
+++ src/redux/features/userSlice.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { User } from "@/types/User/User";
+import Post from "@/components/PostsSection/Post/Post";
+import { RootState } from "../store";
 
 interface initialState {
     isLoading: boolean,
@@ -26,6 +28,34 @@ export const fetchUser = createAsyncThunk('user/fetchUser',
         return user;
     })
 
+export const addUserPost = createAsyncThunk('user/addUserPost',
+    async (post: Post, thunkAPI) => {
+
+        const { userReducer } = thunkAPI.getState() as RootState;
+        const email = userReducer.user?.email;
+        const lastPostId = userReducer.user?.posts.length || 0;
+
+        const newRetweetsAmount = post.retweets.amount + 1;
+
+        post = {
+            ...post,
+            id: lastPostId + 1,
+            retweets: {
+                amount:newRetweetsAmount, 
+                active: true
+            }
+        }
+
+        const response = await fetch('api/user/addPost', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, post })
+        });
+
+        const { status } = await response.json();
+        return post;
+    })
+
 const userSlice = createSlice({
     name: "user",
     initialState,
@@ -41,6 +71,22 @@ const userSlice = createSlice({
         });
 
         builder.addCase(fetchUser.rejected, (state) => {
+            state.error = true;
+            state.isLoading = false;
+        });
+
+        builder.addCase(addUserPost.pending, (state) => {
+            state.isLoading = true;
+        });
+
+        builder.addCase(addUserPost.fulfilled, (state, action) => {
+            state.isLoading = false;
+            if (state.user) {
+                state.user.posts = [action.payload, ...state.user.posts]
+            }
+        });
+
+        builder.addCase(addUserPost.rejected, (state) => {
             state.error = true;
             state.isLoading = false;
         });
