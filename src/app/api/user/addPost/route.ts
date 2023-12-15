51dfad59b38df30ea87039d14ce28b7fe0dc2fbc
Matias Vargas <mat.dweb28[1>
2023-12-15 00:40:03 -0600

    feat(api/user/addPost): add new endpoint for adding posts

---
+++ src/app/api/user/addPost/route.ts
@@ -0,0 +1,29 @@
+import { connectMongoDB } from "@/lib/mongoDB/mongodb";
+import User from "@/models/user";
+
+export async function POST(request: Request) {
+    const {
+        email,
+        post
+    } = await request.json();
+
+    try {
+        await connectMongoDB();
+
+        const user = await User.findOneAndUpdate(
+            { email: email },
+            { $push: { posts: post } },
+            { new: true, runValidators: true }
+        );
+
+        if (user) {
+            return Response.json({ status: 201, statusText: 'Post added successfully' });
+        } else {
+            return Response.json({ status: 400, statusText: 'There was an error adding the post :(' });
+        }
+
+    } catch (e) {
+        console.log(e);
+        return Response.json({ status: 500, statusText: 'Internal Server Error' });
+    }
+}
