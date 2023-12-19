45379464b9d87e569dcfe2378584b0bc39e951d7
mat_dweb <mat.dweb28@gmail.com>
2023-12-18 18:31:20 -0600

    feat(Post): Integrate Retweet component to Post

--- src/components/PostsSection/Post/Post.tsx
+++ src/components/PostsSection/Post/Post.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { bottomPostOptions } from "./BottomPostOptions";
 import UserImage from "../../UserImage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Post } from "@/types/posts/Posts";
 import Image from "next/image";
 import PostOption from "./PostOption";
 import Loader from "@/components/Loaders/Loader";
-import { useAppDispatch } from "@/redux/hook";
+import { useAppSelector, useAppDispatch } from "@/redux/hook";
 import { toggleLikePost } from "@/redux/features/postsSlice";
 import { toggleRetweetPost } from "@/redux/features/postsSlice";
 import { addUserPost } from "@/redux/features/userSlice";
+import { FaRetweet } from "react-icons/fa6";
+import Retweet from "./Retweet";
 
 interface Props {
     key?: number,
@@ -18,8 +20,12 @@ interface Props {
 }
 
 function Post({ postContent, onLoad }: Props) {
+    const user = useAppSelector((state) => state.userReducer.user);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const dispatch = useAppDispatch();
+
+    const [name, setName] = useState<string>('Unauthenticated');
+    const [username, setUsername] = useState<string>('username');
 
     const handleClick = (idClicked: number, name: string) => {
         if (name === 'likes') {
@@ -43,6 +49,11 @@ function Post({ postContent, onLoad }: Props) {
         setIsLoading(false);
     }
 
+    useEffect(() => {
+        setName(user?.name || "Unauthenticated");
+        setUsername(user?.username || "");
+    }, [user])
+
     return (
         <section className='w-full h-full min-h-[8rem] overflow-visible p-5 flex justify-start items-start flex-row flex-nowrap border-b border-primary-gray dark:border-primary-dark-gray'>
             <div className='w-12 h-full'>
@@ -50,22 +61,53 @@ function Post({ postContent, onLoad }: Props) {
             </div>
             <div className='w-full h-full ml-4 flex justify-between items-start flex-col flex-nowrap'>
                 <div className='flex justify-start items-center flex-row flex-nowrap mb-1'>
-                    <p className='font-bold mr-1'>Name</p>
-                    <p className='font-gray-text'>@username</p>
+                    <p className='font-bold mr-1'>
+                        {
+                            postContent.retweet ?
+                                name
+                                :
+                                postContent.name
+                        }
+                    </p>
+                    <p className='font-gray-text'>
+                        @
+                        {
+                            postContent.retweet ?
+                                username
+                                :
+                                postContent.username
+                        }
+                    </p>
+                    {
+                        postContent.retweet &&
+                        <div className='ml-5 opacity-60 flex justify-start items-center flex-row flex-nowrap'>
+                            <FaRetweet style={{ fontSize: "1.2rem" }} />
+                            <p className='font-bold ml-2'>Retweeted</p>
+                        </div>
+                    }
                 </div>
-                <p>
-                    {postContent.title}
-                    <br />
-                    <br />
-                    {postContent.body}
-                </p>
                 {
-                    isLoading &&
-                    <div className='w-full h-[20rem] flex justify-center items-center'>
-                        <Loader className="w-12 h-12" />
-                    </div>
+                    postContent.retweet ?
+                        <div className='w-full scale-90 flex justify-end items-center'>
+                            <Retweet postContent={postContent} />
+                        </div>
+                        :
+                        <>
+                            <p>
+                                {postContent.title}
+                                <br />
+                                <br />
+                                {postContent.body}
+                            </p>
+                            {
+                                isLoading &&
+                                <div className='w-full h-[20rem] flex justify-center items-center'>
+                                    <Loader className="w-12 h-12" />
+                                </div>
+                            }
+                            <Image src={postContent.imageURL || ""} width={500} height={0} className='mt-4 rounded-3xl' alt="post-image" onLoad={() => handleLoadImage()} />
+                        </>
                 }
-                <Image src={postContent.imageURL || ""} width={500} height={0} className='mt-4 rounded-3xl' alt="post-image" onLoad={() => handleLoadImage()} />
                 <div className='w-full max-w-[16rem] sm:max-w-[29rem] flex justify-between items-center flex-row flex-nowrap mt-5 text-primary-dark-gray dark:text-primary-gray'>
                     {bottomPostOptions.map((option) => {
                         return (
