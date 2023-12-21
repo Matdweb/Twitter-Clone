6254238a156c81d9d1fb2afb86ee2325edd48316
Matias Vargas <mat.dweb28[1>
2023-12-20 22:32:53 -0600

    feat: edit TweetButton component to redirect to /home/createPost route

--- src/components/Buttons/TweetButton.tsx
+++ src/components/Buttons/TweetButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useAppSelector } from '@/redux/hook';
 import { TbPencilPlus } from 'react-icons/tb';
+import { useRouter } from 'next/navigation';
 
 type ButtonProps = React.ComponentProps<"button">;
 
@@ -8,8 +9,17 @@ function TweetButton({ className }: ButtonProps) {
     const windowWidth = useAppSelector(state => state.windowWidth);
     const responsiveMenu = useAppSelector(state => state.responsiveMenu);
 
+    const router = useRouter();
+
+    const handleRedirect = () => {
+        router.push('/home/createPost')
+    }
+
     return (
-        <button className={`btn-primary mt-4 p-4 flex justify-center items-center text-white dark:text-white ${className}`}>
+        <button
+            className={`btn-primary mt-4 p-4 flex justify-center items-center text-white dark:text-white ${className}`}
+            onClick={handleRedirect}
+        >
             {windowWidth > 1024 || responsiveMenu ?
                 'Tweet' :
                 <TbPencilPlus style={{ fontSize: "1rem" }} />}
