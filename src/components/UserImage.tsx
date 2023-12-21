8bf5f20e29a0262c851393d66e80f510b0259076
Matias Vargas <mat.dweb28[1>
2023-12-20 22:31:18 -0600

    fix: update UserImage on username change

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -23,7 +23,7 @@ function UserImage({ className, src, username = "Unauthenticated", onClick }: Pr
                     "";
 
         setImgSrc(update);
-    }, [src, user])
+    }, [src, user, username])
 
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -32,7 +32,7 @@ function UserImage({ className, src, username = "Unauthenticated", onClick }: Pr
             <>
                 {
                     isLoading &&
-                    <div className='w-full h-full bg-white dark:bg-black flex justify-center items-center rounded-full' onClick={onClick}>
+                    <div className='w-full h-full bg-white dark:bg-black flex justify-center items-center rounded-full'>
                         <Loader className='w-12 h-12' />
                     </div>
                 }
@@ -48,7 +48,7 @@ function UserImage({ className, src, username = "Unauthenticated", onClick }: Pr
             </>
         )
     } else {
-        const firstLetter = username?.charAt(0).toUpperCase();
+        const firstLetter = username.charAt(0).toUpperCase();
         return (
             <div
                 className={`${className} rounded-full flex justify-center items-center border border-white bg-primary-gray`}
