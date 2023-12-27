21ef812d461ea026f879009f53da654f1038ecb2
Matias Vargas <mat.dweb28[1>
2023-12-26 23:03:20 -0600

    fix: render Loader when there's an image

--- src/components/PostsSection/Post/Retweet.tsx
+++ src/components/PostsSection/Post/Retweet.tsx
@@ -29,14 +29,17 @@ function Retweet({ postContent }: Props) {
                     {postContent.body}
                 </p>
                 {
-                    isLoading &&
-                    <div className='w-full h-[12rem] flex justify-center items-center'>
-                        <Loader className="w-12 h-12" />
-                    </div>
-                }
-                {
                     postContent.imageURL &&
-                    <Image src={postContent.imageURL || ""} width={300} height={0} className='mt-4 rounded-3xl' alt="post-image" onLoad={() => setIsLoading(false)} />
+                    <>
+                        {
+                            isLoading &&
+                            <div className='w-full h-[12rem] flex justify-center items-center'>
+                                <Loader className="w-12 h-12" />
+                            </div>
+                        }
+
+                        <Image src={postContent.imageURL || ""} width={300} height={0} className='mt-4 rounded-3xl' alt="post-image" onLoad={() => setIsLoading(false)} />
+                    </>
                 }
             </div>
         </section>
