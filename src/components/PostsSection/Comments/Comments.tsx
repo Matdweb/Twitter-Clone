3bb96c75f463fbe9bfe9b019c04b736ad50520bc
Matias Vargas <mat.dweb28[1>
2023-12-26 22:00:27 -0600

    feat: create Comments component to render an array of comments

---
+++ src/components/PostsSection/Comments/Comments.tsx
@@ -0,0 +1,27 @@
+import UserImage from "@/components/UserImage"
+import type { comment } from "@/types/posts/comments/comment"
+
+interface Props {
+    comments: comment[]
+}
+
+function Comments({ comments }: Props) {
+    return (
+        <section className='w-full overflow-hidden overflow-y-scroll max-h-[16rem]'>
+            {comments?.map((comment) => {
+                return (
+                    <div key={comment.id} className='w-full px-5 py-3 flex justify-start items-center'>
+                        <div className='mr-4'>
+                            <UserImage className='w-10 h-10' username={comment.username} />
+                        </div>
+                        <div>
+                            <p>{comment.body}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
+
+export default Comments
