70712eaec5f09c2f28fd793e2c206aba4fa02880
Matias Vargas <mat.dweb28[1>
2023-12-26 22:01:30 -0600

    feat: create addComment redux action in posts slice

--- src/redux/features/postsSlice.ts
+++ src/redux/features/postsSlice.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { Post } from "@/types/posts/Posts";
+import type { comment } from "@/types/posts/comments/comment";
+import type { RootState } from "../store";
 
 type PostsState = {
     isLoading: boolean,
@@ -24,6 +26,38 @@ export const fecthPosts = createAsyncThunk('posts/fetchPosts',
         const { posts } = await response.json();
 
         return posts;
+    })
+
+export const addComment = createAsyncThunk('posts/addComment',
+    async ({ postId, comment }: { postId: number, comment: comment }, thunkAPI) => {
+        const { userReducer: { user }, postsReducer: { posts } } = thunkAPI.getState() as RootState;
+
+        const [postToComment] = posts.filter((post) => {
+            return post.id === postId;
+        })
+
+        comment = {
+            ...comment,
+            id: postToComment.comments.length > 0 ? postToComment.comments[postToComment.comments.length - 1].id + 1 : 0,
+            username: user?.name || "Name",
+        };
+
+        if (postId > 100) {
+            const response = await fetch('/api/posts/addComment', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email: user?.email || "", postId, comment })
+            });
+
+            const { status } = await response.json();
+            if (status !== 201) return null;
+        }
+
+        return {
+            ...postToComment,
+            comments: [...postToComment.comments, comment]
+        };
+
     })
 
 const postsSlice = createSlice({
@@ -84,6 +118,30 @@ const postsSlice = createSlice({
         });
 
         builder.addCase(fecthPosts.rejected, (state) => {
+            state.error = true;
+            state.isLoading = false;
+        });
+
+        builder.addCase(addComment.pending, (state) => {
+            state.isLoading = true;
+        });
+
+        builder.addCase(addComment.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            if (payload !== null) {
+                state.posts = state.posts.map((post) => {
+                    if (payload.id === post.id) {
+                        return {
+                            ...payload
+                        }
+                    } else {
+                        return post;
+                    }
+                })
+            }
+        });
+
+        builder.addCase(addComment.rejected, (state) => {
             state.error = true;
             state.isLoading = false;
         });
