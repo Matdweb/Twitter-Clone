b3f8ee7b9e8c5833237c7375772b0b9e64281e93
Matias Vargas <mat.dweb28[1>
2023-12-30 00:22:13 -0600

    feat(/api/posts/addComment): update /addComment endpoint to search user by userId

--- src/app/api/posts/addComment/route.ts
+++ src/app/api/posts/addComment/route.ts
@@ -1,20 +1,36 @@
 import { connectMongoDB } from "@/lib/mongoDB/mongodb";
 import User from "@/models/user";
+import type { Post } from "@/types/posts/Posts";
 
 export async function POST(request: Request) {
-    const { email, postId, comment } = await request.json();
+    const { userId, postId, comment } = await request.json();
 
     try {
         await connectMongoDB();
-        const user = await User.findOneAndUpdate(
-            { email, 'posts.id': postId },
-            {
-                $push: {
-                    'posts.$.comments': comment
-                }
-            },
-            { new: true, runValidators: true }
-        );
+        const user = await User.findOne({ _id: userId });
+
+        if (!user) {
+            return Response.json({ status: 400, statusText: 'There was an error finding the user :(' });
+        }
+
+        const postToUpdate: Post = user?.posts.find((post: Post) => post.id === postId);
+
+        if (!postToUpdate) {
+            return Response.json({ status: 400, statusText: 'There was an error finding the post :(' });
+        }
+
+        const newComment = {
+            ...comment,
+            id: postToUpdate.comments.length > 0 ? postToUpdate.comments[postToUpdate.comments.length - 1].id + 1 : 0,
+        };
+
+        postToUpdate.comments.push(newComment);
+
+        await user.save();
+
+        if (postToUpdate) {
+            return Response.json({ status: 201, statusText: 'User updated successfully', post: postToUpdate });
+        }
 
         if (user) {
             return Response.json({ status: 201, statusText: 'User updated successfully' });
