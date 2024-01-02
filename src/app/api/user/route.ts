e9e03ba2c22f484112dec6041f41067abfebf337
Matias Vargas <mat.dweb28[1>
2024-01-02 00:09:31 -0600

    feat(api/user): return user response with correct likes structure

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,9 +3,30 @@ import findUserByEmail from "@/lib/mongoDB/findUserByEmail";
 export async function POST(request: Request) {
     const { email } = await request.json();
 
-    const user = await findUserByEmail(email)
+    const user = await findUserByEmail(email);
 
     if (user) {
+        user.posts = user.posts.map((post: { likes: string[] }) => {
+            const likesAmount = post.likes.length;
+            if (post.likes.includes(user._id)) {
+                return {
+                    ...post,
+                    likes: {
+                        amount: likesAmount,
+                        active: true
+                    }
+                }
+            } else {
+                return {
+                    ...post,
+                    likes: {
+                        amount: likesAmount,
+                        active: false
+                    }
+                }
+            }
+        })
+
         return Response.json({ status: 201, statusText: 'successfull', user });
     } else {
         return Response.json({ status: 400, statusText: 'Something went wrong :(', user: null });
