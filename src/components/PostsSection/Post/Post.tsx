7ea0a37fd0e445889fc154aa3627037447c5104d
mat_dweb <mat.dweb28@gmail.com>
2024-01-04 01:05:31 -0600

    feat(Post): modify the Like funcionality to work with new structure in Post component

--- src/components/PostsSection/Post/Post.tsx
+++ src/components/PostsSection/Post/Post.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import PostOption from "./PostOption";
 import Loader from "@/components/Loaders/Loader";
 import { useAppSelector, useAppDispatch } from "@/redux/hook";
+import { toggleLikeUserPost } from "@/redux/features/userSlice";
 import { toggleLikePost } from "@/redux/features/postsSlice";
 import { FaRetweet } from "react-icons/fa6";
 import Retweet from "./Retweet";
@@ -32,23 +33,34 @@ function Post({ postContent, onLoad, options = true }: Props) {
     const [retweetModal, setRetweetModal] = useState<boolean>(false);
     const [commentModal, setCommentModal] = useState<boolean>(false);
 
-    const handleClick = (idClicked: number, name: string) => {
-        if (name === 'likes') {
-            handleLikePost(idClicked);
-        } else if (name === 'retweets') {
+    const handleClick = (optionName: string) => {
+        if (optionName === 'likes') {
+            handleLikePost();
+        } else if (optionName === 'retweets') {
             if (!postContent.retweets.active) {
                 setRetweetModal(true);
             }
-        } else if (name === 'comments') {
+        } else if (optionName === 'comments') {
             setCommentModal(true);
         }
     }
 
-    const handleLikePost = (id: number) => {
+    const handleLikePost = () => {
+        //toggle like in redux state 
         dispatch(toggleLikePost({
-            userId: postContent.userId,
+            userId: user?._id || '',
             postId: postContent.id
         }));
+        //toggle like in data base and redux if necessary
+        if (postContent.id > 100) {
+            dispatch(toggleLikeUserPost({
+                userId: user?._id || '',
+                post: {
+                    id: postContent.id,
+                    userId: postContent.userId
+                }
+            }));
+        }
     }
 
     const handleLoadImage = () => {
@@ -140,14 +152,14 @@ function Post({ postContent, onLoad, options = true }: Props) {
                             }
                         </>
                 }
-                <div className={`w-full max-w-[16rem] sm:max-w-[29rem] flex justify-between items-center flex-row flex-nowrap mt-5 text-primary-dark-gray dark:text-primary-gray ${postContent.retweet && `opacity-40 cursor-not-allowed`} ${!options || !user && `opacity-40 cursor-not-allowed`}`} onClick={handleDisabledOptions}>
+                <div className={`w-full max-w-[16rem] sm:max-w-[29rem] flex justify-between items-center flex-row flex-nowrap mt-5 text-primary-dark-gray dark:text-primary-gray ${!options || !user || postContent.retweet ? `opacity-40 cursor-not-allowed` : ''}`} onClick={handleDisabledOptions}>
                     {bottomPostOptions.map((option) => {
                         return (
                             <PostOption
                                 key={option.id}
                                 option={option}
                                 post={postContent}
-                                handleClick={() => !postContent.retweet && options ? handleClick(postContent.id, option.name) : ''}
+                                handleClick={() => !(postContent.retweet) && options ? handleClick(option.name) : ''}
                             />
                         )
                     })}
