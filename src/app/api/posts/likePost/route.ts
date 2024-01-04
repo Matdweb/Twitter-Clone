cf28899a43b7a5b9bab41d6581485f3bfa6eba26
mat_dweb <mat.dweb28@gmail.com>
2024-01-03 19:50:21 -0600

    feat: modify the api/posts/likePost endpoint to modify the new schemas

--- src/app/api/posts/likePost/route.ts
+++ src/app/api/posts/likePost/route.ts
@@ -3,42 +3,49 @@ import User from "@/models/user";
 import type { Post } from "@/types/posts/Posts";
 
 export async function POST(request: Request) {
-    const { userId, postId } = await request.json();
+    const {
+        userId, post
+    }: {
+        userId: string,
+        post: { id: number, userId: string }
+    } = await request.json();
 
     try {
         await connectMongoDB();
-        const user = await User.findOne({ _id: userId });
+        const user = await User.findOne({ _id: post.userId });
 
         if (!user) {
-            return Response.json({ status: 400, statusText: 'There was an error finding the user :(', post: null });
+            return Response.json({ status: 400, statusText: 'There was an error finding the user :(', likedPost: null });
         }
 
-        const postToUpdate = user.posts.find((post: { id: string, likes: string[] }) => post.id === postId);
+        const postToUpdate: Post = user.posts.find((userPost: Post) => userPost.id === post.id);
 
         if (!postToUpdate) {
-            return Response.json({ status: 400, statusText: 'There was an error finding the post :(', post: null });
+            return Response.json({ status: 400, statusText: 'There was an error finding the post :(', likedPost: null });
         }
 
-        const activeUserLike: boolean = postToUpdate.likes.includes(userId)
+        const activeUserLike: boolean = postToUpdate.likes.userIds.includes(userId) //userId of the one that likes the post not the owner
 
         if (activeUserLike) {
-            postToUpdate.likes = postToUpdate.likes.filter((user: string) => {
-                if (user !== userId) {
-                    return user;
+            postToUpdate.likes.userIds = postToUpdate.likes.userIds.filter((user_id: string) => {
+                if (user_id !== userId) {
+                    return user_id;
                 }
             })
+            postToUpdate.likes.amount -= 1;
         } else {
-            postToUpdate.likes.push(userId);
+            postToUpdate.likes.userIds.push(userId);
+            postToUpdate.likes.amount += 1;
         }
 
         await user.save();
 
         if (postToUpdate) {
-            return Response.json({ status: 201, statusText: 'User updated successfully', post: postToUpdate, likes: postToUpdate.likes.length, active: !activeUserLike });
+            return Response.json({ status: 201, statusText: 'User updated successfully', likedPost: postToUpdate });
         }
 
     } catch (e) {
         console.log(e);
-        return Response.json({ status: 500, statusText: 'Internal Server Error', post: null });
+        return Response.json({ status: 500, statusText: 'Internal Server Error', likedPost: null });
     }
 }
