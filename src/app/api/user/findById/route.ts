201275bcd081193ce1dc553afc49b81b692824e9
Matias Vargas <mat.dweb28[1>
2024-01-04 23:06:43 -0600

    feat: create a api/user/findById endpoint that returns the user object from the data base

---
+++ src/app/api/user/findById/route.ts
@@ -0,0 +1,21 @@
+import User from "@/models/user";
+import { connectMongoDB } from "@/lib/mongoDB/mongodb";
+
+export async function POST(request: Request) {
+    const { userId } = await request.json();
+
+    try {
+        await connectMongoDB();
+        const user = await User.findOne({ _id: userId });
+
+        if (user) {
+            return Response.json({ status: 201, statusText: 'successfull', user });
+        } else {
+            return Response.json({ status: 400, statusText: 'Something went wrong :(', user: null });
+        }
+
+    } catch (e) {
+        console.log(e);
+        return Response.json({ status: 400, statusText: 'Internal server error', user: null });
+    }
+}
