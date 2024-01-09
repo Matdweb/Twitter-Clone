e87be72f474c85ad42abaada78b4050646fef2aa
Matias Vargas <mat.dweb28[1>
2024-01-09 00:25:21 -0600

    feat: create the home/more route page

---
+++ src/app/home/more/page.tsx
@@ -0,0 +1,11 @@
+import WorkInProgressGif from "@/components/WorkInProgressGif"
+import TwitterHeader from "@/components/Header/TwitterHeader"
+
+function Page() {
+    return (<>
+        <TwitterHeader section='More' />
+        <WorkInProgressGif />
+    </>)
+}
+
+export default Page
